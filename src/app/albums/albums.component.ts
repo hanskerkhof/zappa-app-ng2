881c45a56a9672809d49f377aeb6cace4697f6a9
Hans Kerkhof <hans@netmatic.nl>
2018-06-01 11:13:20 +0200

    Fix linting

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,111 +1,110 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import {Album, IAlbum} from '../shared/albums/album.model';
-import {AlbumService} from '../shared/albums/album.service';
-import {SpotifyService} from '../shared/spotify.service';
-import {LocalStorageService} from 'angular-2-local-storage';
+import { Album, IAlbum } from '../shared/albums/album.model';
+import { AlbumService } from '../shared/albums/album.service';
+import { SpotifyService } from '../shared/spotify.service';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
-    templateUrl: './albums.component.html',
-    styleUrls: ['./albums.component.css']
+  templateUrl: './albums.component.html',
+  styleUrls: ['./albums.component.css']
 })
 export class AlbumsComponent implements OnInit, OnDestroy {
-    albums$: any;
-    editing: any = {};
-    newalbum: boolean = false;
-    n = new Album('', null, '');
-    albumsList = [];
-    albumsCount: number;
-    subscriber: any;
-    displayMode: string = 'list';
-    salbum: any = {};
-    sralbum: any = {};
+  albums$: any;
+  editing: any = {};
+  newalbum: boolean = false;
+  n = new Album('', null, '');
+  albumsList = [];
+  albumsCount: number;
+  subscriber: any;
+  displayMode: string = 'list';
+  salbum: any = {};
+  sralbum: any = {};
 
-    constructor(public albumService: AlbumService,
-                public SpotifyService: SpotifyService,
-                private localStorageService: LocalStorageService) {
-    }
+  constructor(public albumService: AlbumService,
+              public SpotifyService: SpotifyService,
+              private localStorageService: LocalStorageService) {
+  }
 
-    createAlbum(album: IAlbum): void {
-        let r = this.albumService.createAlbum(album);
-        r.then(() => {
-            this.newalbum = false;
-            this.n = new Album('', null, '');
-        });
-    }
+  createAlbum(album: IAlbum): void {
+    let r = this.albumService.createAlbum(album);
+    r.then(() => {
+      this.newalbum = false;
+      this.n = new Album('', null, '');
+    });
+  }
 
-    setDisplayMode(mode: string): void {
-        this.localStorageService.set('displayMode', mode);
-        this.editing = {};
-        this.displayMode = mode;
-    }
+  setDisplayMode(mode: string): void {
+    this.localStorageService.set('displayMode', mode);
+    this.editing = {};
+    this.displayMode = mode;
+  }
 
-    setOwned(album: IAlbum): void {
-        let changes: IAlbum = {
-            name: album.name.trim(),
-            year: album.year,
-            owned: album.owned,
-            spotifyAlbumId: album.spotifyAlbumId,
-            uuid: album.uuid
-        };
-        this.albumService.updateAlbum(album, changes);
-    }
+  setOwned(album: IAlbum): void {
+    let changes: IAlbum = {
+      name: album.name.trim(),
+      year: album.year,
+      owned: album.owned,
+      spotifyAlbumId: album.spotifyAlbumId,
+      uuid: album.uuid
+    };
+    this.albumService.updateAlbum(album, changes);
+  }
 
-    updateAlbum(album: IAlbum): void {
-        let changes: IAlbum = {
-            name: album.name.trim(),
-            year: album.year,
-            owned: album.owned,
+  updateAlbum(album: IAlbum): void {
+    let changes: IAlbum = {
+      name: album.name.trim(),
+      year: album.year,
+      owned: album.owned,
 //            3PZXB9NBWf11eDS72JCGaY
-            spotifyAlbumId: album.spotifyAlbumId,
-            uuid: album.uuid
-        };
-        this.albumService.updateAlbum(album, changes)
-            .then(() => {
-                this.editing = {};
-            }); // returns a promise
+      spotifyAlbumId: album.spotifyAlbumId,
+      uuid: album.uuid
+    };
+    this.albumService.updateAlbum(album, changes)
+      .then(() => {
+        this.editing = {};
+      }); // returns a promise
+  }
+
+  deleteAlbum(album: IAlbum): void {
+    let a = confirm('Really delete album ' + album.name + '?');
+    if (a) {
+      this.albumService.deleteAlbum(album);
     }
+  }
 
-    deleteAlbum(album: IAlbum): void {
-        let a = confirm('Really delete album ' + album.name + '?');
-        if (a) {
-            this.albumService.deleteAlbum(album);
-        }
-    }
-
-    searchSpotifyAlbum (albumName) {
-        this.SpotifyService.searchAlbum(albumName)
-            .subscribe(
-                sralbum => this.sralbum = sralbum, //Bind to view
-                err => {
-                    // Log errors if any
-                });
-
-    }
-
-    getSpotifyAlbum (albumId: string) {
-        this.SpotifyService.getAlbum(albumId)
-            .subscribe(
-                salbum => this.salbum = salbum, //Bind to view
-                err => {
-                    // Log errors if any
-                    console.log(err);
-                });
-    }
-
-    ngOnInit(): void {
-        if (this.localStorageService.get('displayMode')) {
-            this.displayMode = this.localStorageService.get('displayMode').toString();
-        }
-
-        this.albums$ = this.albumService.listAlbums();
-        this.subscriber = this.albumService.listAlbums().subscribe((albumsList) => {
-            this.albumsCount = albumsList.length;
-            this.albumsList = albumsList;
+  searchSpotifyAlbum(albumName: string): void {
+    this.SpotifyService.searchAlbum(albumName)
+      .subscribe(
+        sralbum => this.sralbum = sralbum, // Bind to view
+        err => {
+          // Log errors if any
         });
+  }
+
+  getSpotifyAlbum(albumId: string): void {
+    this.SpotifyService.getAlbum(albumId)
+      .subscribe(
+        salbum => this.salbum = salbum, // Bind to view
+        err => {
+          // Log errors if any
+          console.log(err);
+        });
+  }
+
+  ngOnInit(): void {
+    if (this.localStorageService.get('displayMode')) {
+      this.displayMode = this.localStorageService.get('displayMode').toString();
     }
 
-    ngOnDestroy(): void {
-        this.subscriber.unsubscribe();
-    }
+    this.albums$ = this.albumService.listAlbums();
+    this.subscriber = this.albumService.listAlbums().subscribe((albumsList) => {
+      this.albumsCount = albumsList.length;
+      this.albumsList = albumsList;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriber.unsubscribe();
+  }
 }
--- src/app/shared/spotify.service.ts
+++ src/app/shared/spotify.service.ts
@@ -2,42 +2,42 @@
 
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/switchMap';
-import {Injectable} from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 // import {AngularFire, FirebaseListObservable} from 'angularfire2';
 // import {IAlbum, Album, AlbumData} from './album.model';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class SpotifyService {
-    public items: any;
+  public items: any;
+
 //    const albums$: FirebaseListObservable<IAlbum[]>;
 
-    constructor(private http: Http) {
-        // const url = 'https://api.spotify.com/v1';
+  constructor(private http: Http) {
+    // const url = 'https://api.spotify.com/v1';
 
-        // this.albums$ = af.database.list(path,
-        //     {
-        //         query: {
-        //             orderByChild: 'year'
-        //         }
-        //     });
-        // this.items = af.database.list('/zappa-discography');
-    }
+    // this.albums$ = af.database.list(path,
+    //     {
+    //         query: {
+    //             orderByChild: 'year'
+    //         }
+    //     });
+    // this.items = af.database.list('/zappa-discography');
+  }
 
-    public searchAlbum(name): Observable<Comment[]> {
-        return this.http.get('https://api.spotify.com/v1/search/?q=' + name + '&type=album')
-            .map((res: Response) => res.json())
-            //...errors if any
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
+  public searchAlbum(name: string): Observable<Comment[]> {
+    return this.http.get('https://api.spotify.com/v1/search/?q=' + name + '&type=album')
+      .map((res: Response) => res.json())
+      // ...errors if any
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
 
-    public
-    getAlbum(id): Observable < Comment[] > {
-        return this.http.get('https://api.spotify.com/v1/albums/' + id)
-            .map((res: Response) => res.json())
-            //...errors if any
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
+  public getAlbum(id: string|number): Observable<Comment[]> {
+    return this.http.get('https://api.spotify.com/v1/albums/' + id)
+      .map((res: Response) => res.json())
+      // ...errors if any
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
 }
